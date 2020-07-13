bf2cf9db6c5df50975d4d6d560a74ba09a018956
lhd252257 <252257036@qq.com>
2020-07-13 11:29:12 +0800

    修改MapperScan

--- src/main/java/com/bifangan/dmDemo/DmDemoApplication.java
+++ src/main/java/com/bifangan/dmDemo/DmDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.bifangan.dmDemo.common.UUIDUtils;
-@MapperScan("com.bifangan.dmDemo")
+@MapperScan("com.bifangan.dmDemo.mapper")
 @SpringBootApplication
 @CrossOrigin("*")
 @EnableScheduling
